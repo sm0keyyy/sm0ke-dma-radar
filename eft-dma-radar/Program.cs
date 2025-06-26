d677c06acca44312918af4ed45d27a5ccdb0d597
x0m <63579245+xx0m@users.noreply.github.com>
2025-06-26 13:21:45 +1200

    Updated eft program version

--- eft-dma-radar/Program.cs
+++ eft-dma-radar/Program.cs
@@ -54,7 +54,7 @@ namespace eft_dma_radar
     internal static class Program
     {
         internal const string Name = "EFT DMA Radar";
-        internal const string Version = "1.2.0";
+        internal const string Version = "1.2.1";
 
         /// <summary>
         /// Current application mode
